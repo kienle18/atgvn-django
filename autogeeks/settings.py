631af4bd24333d0cb0a29fdb312584d7f2b665e0
Le Trung Kien <trungkien.le1908@gmail.com>
2019-05-26 08:38:08 -0400

    Add static files

--- autogeeks/settings.py
+++ autogeeks/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog',
 ]
 
 MIDDLEWARE = [
@@ -119,4 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = 'staticfiles'
+STATICFILEES_DIRS = {
+    os.path.join(BASE_DIR, 'static')
+}
